84613522b99db1f420bf4d275dcb59243c380d33
Wick282 <nwickremesekara@gmail.com>
2025-05-21 22:09:01 +0530

    commit PhotographerServlet.java to backend

--- src/main/java/photographer/nav/PhotographerServlet.java
+++ src/main/java/photographer/nav/PhotographerServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.*;
 
 import java.io.IOException;
 import java.util.List;
-
+//c
 @WebServlet(name = "PhotographerServlet", urlPatterns = {
         "/listPhotographers",
         "/addPhotographer",
