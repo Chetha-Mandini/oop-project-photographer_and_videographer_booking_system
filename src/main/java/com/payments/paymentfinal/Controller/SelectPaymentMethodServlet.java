9b50f9dc3ca57e815dedcff2902944640ea4fb08
Ias2A <imandi2.a.s@gmail.com>
2025-05-21 22:51:34 +0530

    Commit 5 from IT24100093

--- src/main/java/com/payments/paymentfinal/Controller/SelectPaymentMethodServlet.java
+++ src/main/java/com/payments/paymentfinal/Controller/SelectPaymentMethodServlet.java
@@ -28,5 +28,6 @@ public class SelectPaymentMethodServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.sendRedirect("SelectPaymentMethod.jsp");
+
     }
 }
